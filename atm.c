6678a3c53a2d00486e90a858308c95ff57c23707
vaishnavi <58027647+imvaishu@users.noreply.github.com>
2020-04-07 20:57:43 +0530

    modified atm.c

--- atm.c
+++ atm.c
@@ -3,17 +3,16 @@
 unsigned int get_money(unsigned short int amount)
 {
   unsigned short int total_amount = amount;
-  if(total_amount > 31999)
-  {
-    return 0;
-  }
   unsigned short int denominations[8] = {2000,500,100,50,20,10,5,1};
   unsigned num_of_notes = 0;
 
-  for(int i = 0 ; i < 8 ; i++)
-  {
-    num_of_notes =  num_of_notes << 4 | total_amount / denominations[i];
-    total_amount = total_amount % denominations[i];
+  if(total_amount <= 31999)
+  { 
+    for(int index = 0 ; index < 8 ; index++)
+    {
+      num_of_notes =  num_of_notes << sizeof(unsigned) | total_amount / denominations[index];
+      total_amount = total_amount % denominations[index];
+    }
   }
   return num_of_notes;
 }
